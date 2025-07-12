2150f0cc0bad20c57c420c6b1cbad77c7b6aa260
Lin872 <50880594+Lin872@users.noreply.github.com>
2025-07-12 09:47:49 +0800

    Update GTSUploader.user.js
    移除攻波上傳

--- GTSUploader.user.js
+++ GTSUploader.user.js
@@ -4,8 +4,6 @@
 // @version      1.0
 // @description  try to take over the world!
 // @author       You
-// @include      *://*.travian.*/build.php*gid=16*tt=1*
-// @include      *://*.travian.*/build.php*tt=1*gid=16*
 // @include      *://*.travian.*/profile/*
 // @grant        none
 // ==/UserScript==
